16f86f601dbd54609a2ca7231bfa3532ee600695
Nico De Witte <nico.dewitte@vives.be>
2020-10-31 09:08:21 +0100

    Add code

---
+++ ForLoopsGroup2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ForLoopsGroup2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("i = " + i);
+            }
+
+            int k = 0;
+            for ( ; k < 10; k++)
+            {
+
+            }
+            Console.WriteLine("Last k = " + k);
+
+            for (int j = 10; j >= 0; j--)
+            {
+                Console.WriteLine("j = " + j);
+            }
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - For Loops - Group 2
+
+Demo code of for-loops.
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/ForLoopsGroup2.git
+```
